def4281c691c16b87754044f41732ff48fbfe060
Colin Bradford <cjthehippo@gmail.com>
2025-05-14 11:04:49 -0700

    updating, and including new play/pause version

---
+++ carousel/concerns.md
@@ -0,0 +1,15 @@
+1. Keyboard Accessibility:
+- Concern: Carousels often lack proper keyboard navigation support, making it difficult for users who rely on keyboards or other assistive devices to interact with and control the content.
+- Mitigation: Ensure comprehensive keyboard navigation support, allowing users to navigate through carousel content easily. Focus management should be implemented, ensuring that interactive elements within the carousel are accessible via keyboard.
+2. Auto-Playing Carousels:
+- Concern: Auto-Playing carousels (automatically switches slides) can be disorientating, distracting, difficult to read and operate. 
+- Mitigation: Provide users with controls to pause, play, and navigate through the carousel manually, reducing the reliance on automatic timing. Additionally, allow users to control the speed of transitions or disable automatic movement.
+4. Responsive Design Issues:
+- Concern: Carousels may not adapt well to different screen sizes, causing issues on smaller devices and reducing the overall user experience.
+- Mitigation: Implement responsive design principles to ensure that carousels adapt seamlessly to various screen sizes. Test the carousel on different devices to verify its usability and readability.
+5. Alternative Content Accessibility:
+- Concern: Providing alternative content, such as descriptive text or links, for each carousel slide may be overlooked, impacting users who cannot view the visual content.
+- Mitigation: Include descriptive text or links for each carousel slide, ensuring that users with disabilities or those who have opted not to view visual content can access relevant information.
+7. Operable Timeouts:
+- Concern: Timed interactions within carousels, such as automatic transitions, may not account for users who require more time to read or interact with the content.
+- Mitigation: Allow users to adjust the timing of carousel transitions or provide an option to pause automatic movements, ensuring that users have adequate time to interact with the content.
---
+++ carousel/play-pause/main.js
@@ -0,0 +1,164 @@
+
+// encapsulate so that we don't mess with the global context
+(() => {
+    main();
+
+    function main() {
+        const context = document.getElementById('carousel');
+        
+        const dots = [...context.querySelectorAll('.dot')];
+        const slides = [...context.querySelectorAll('.carousel-slide')];
+        const prevSlideButton = context.querySelector('.prev');
+        const nextSlideButton = context.querySelector('.next');
+        const liveRegion = context.querySelector('[role="status"]');
+        
+        const playPause = context.querySelector('[data-alternate]');
+        playPause.addEventListener(
+            'click', 
+            toggleAlternate
+        );
+    
+        addArrowKeyNavigation(dots);
+        for(let dot of dots) {
+            dot.addEventListener(
+                'click', 
+                () => activateDot(dot, dots, slides, liveRegion)
+            );
+        }
+        prevSlideButton.addEventListener(
+            'click', 
+            () => getNextDot(dots, -1).click()
+        );
+        nextSlideButton.addEventListener(
+            'click', 
+            () => getNextDot(dots,  1).click()
+        );
+
+        autoPlayCarousel(
+            dots, 
+            () => playPause.dataset.alternate === 'play', 
+            2000
+        );
+    }
+    
+    function activateDot(dot, dots, slides, liveRegion) {
+        // set pressed value
+        for(let i = 0; i < dots.length; i++) {
+            let dot = dots[i];
+            let slide = slides[i];
+            dot.setAttribute('aria-pressed', 'false');
+            slide.setAttribute('aria-hidden', 'true');
+        }
+        let slide = slides[dots.indexOf(dot)];
+        dot.setAttribute('aria-pressed', 'true');
+        slide.setAttribute('aria-hidden', 'false');
+        liveRegion.textContent = `${dots.indexOf(dot) + 1}/${dots.length}`;
+    }
+    
+    
+    /**
+     * Add arrow key navigation
+     * @param {HTMLElement[]} items 
+     * @param {('horizontal'|'vertical')[]} directions 
+     */
+    function addArrowKeyNavigation(items, directions = ['horizontal', 'vertical']) {
+        for(let item of items) {
+            item.addEventListener(
+                'keydown', 
+                (e) => arrowNavigationKeyboardHandler(e, item, items, directions)
+            );
+        }
+    }
+    
+    /**
+     * 
+     * @param {KeyboardEvent} e 
+     * @param {HTMLElement} item 
+     * @param {HTMLElement[]} items 
+     * @param {('horizontal'|'vertical')[]} directions 
+     * @returns 
+     */
+    function arrowNavigationKeyboardHandler(e, item, items, directions = ['horizontal', 'vertical']) {
+        // return if not the right key
+        let allowedKeys = [];
+        if (directions.includes('horizontal')) {
+            allowedKeys.push('ArrowLeft', 'ArrowRight');
+        }
+        if (directions.includes('vertical')) {
+            allowedKeys.push('ArrowUp', 'ArrowDown');
+        }
+    
+        if (!allowedKeys.includes(e.key)) return;
+    
+        // set the direction based on arrow key pressed
+        let direction;
+        switch(e.key) {
+            case 'ArrowUp':
+            case 'ArrowLeft':
+                direction = -1;
+                break;
+            case 'ArrowDown':
+            case 'ArrowRight':
+                direction = 1;
+                break;
+        }
+    
+        // get next item and focus it
+        let nextItem = getNext(item, items, direction);
+        nextItem.focus();
+    
+        // prevent browser from scrolling
+        e.preventDefault();
+    }
+    
+    /**
+     * gets next item in list based on direction (1 = next, -1 = previous)
+     * @param {T} item 
+     * @param {T[]} items 
+     * @param {1|-1} direction 
+     * @returns next item in array
+     */
+    function getNext(item, items, direction = 1) {
+        let index = items.indexOf(item);
+        let nextIndex = (index + direction + items.length) % items.length;
+        return items[nextIndex];
+    }
+    
+    /**
+     * 
+     * @param {HTMLElement[]} dots 
+     * @param {(1|-1)} direction 
+     */
+    function getNextDot(dots, direction) {
+        let currentDot = dots.find(d => d.getAttribute('aria-pressed') === 'true');
+        return getNext(currentDot, dots, direction);
+    }
+
+    /**
+     * 
+     * @param {Event} e 
+     */
+    function toggleAlternate(e) {
+        let el = e.currentTarget;
+        let name = el.querySelector('.name');
+        [name.textContent, el.dataset.alternate] =
+        [el.dataset.alternate, name.textContent];
+    }
+
+    /**
+     * 
+     * @param {HTMLElement[]} dots 
+     * @param {() => boolean} whileCondition true = autoplay, false = pause
+     * @param {number} time milliseconds until move
+     * @return timeout
+     */
+    function autoPlayCarousel(dots, whileCondition, time = 5000) {
+        let timeout = setTimeout(() => {
+            if (whileCondition()) {
+                getNextDot(dots, 1).click();
+            }
+            autoPlayCarousel(dots, whileCondition, time);
+        }, time);
+        return timeout;
+    }
+})();
